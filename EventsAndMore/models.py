00e65df8c9b597138c24f5e00c91306e804c8964
DiegoMartinezDeSanjuan <didimdsj@gmail.com>
2022-04-22 19:02:58 +0200

    added method str

--- EventsAndMore/models.py
+++ EventsAndMore/models.py
@@ -42,3 +42,6 @@ class Stand(models.Model):
     id = models.AutoField(primary_key=True)
     occupied = models.BooleanField(blank=False, null=False)
     description = models.CharField(max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.description)
